6c372cde1f30310974409fe668100668b7d5e97b
Michael Llorens <Michaelllorens87@gmail.com>
2024-11-18 20:03:37 +0100

    Delete YouFitness directory

--- YouFitness/app/src/main/java/com/example/youfitness/utils/Constantes.java
+++
@@ -1,7 +0,0 @@
-package com.example.youfitness.utils;
-
-public class Constantes {
-    public static final String C_MODO  = "modo";
-    public static final int C_VISUALIZAR = 551;
-    public static final int C_CREAR = 552;
-}
